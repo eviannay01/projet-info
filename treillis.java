4080ee591cbe76eae3c452b2bc9ffaf91a020d7d
eviannay01 <83212220+eviannay01@users.noreply.github.com>
2021-04-26 11:30:15 +0200

    Add files via upload

---
+++ treillis.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class treillis {
+
+    /**
+     * Default constructor
+     */
+    public treillis() {
+    }
+
+    /**
+     * 
+     */
+    private terrain terrain;
+
+    /**
+     * 
+     */
+    private catalogue barre catalogue;
+
+
+
+
+
+
+}
